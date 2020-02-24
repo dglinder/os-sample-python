eccf64ad14e0f56d512ca7f16ffc94bd2b0c29da
Daniel Linder <dglinder@west.com>
2020-02-24 05:32:15 -0600

    Adding a color route

--- wsgi.py
+++ wsgi.py
@@ -5,5 +5,9 @@ application = Flask(__name__)
 def hello():
     return "Hello World 001!"
 
+@application.route("/color")
+def hello():
+    return "The color is: XXX"
+
 if __name__ == "__main__":
     application.run()
